94e26d05d853120a40d762b50462f534c40b029a
Alexandr Borisov <aoborisov@gmail.com>
2022-03-21 08:56:21 +0300

    Start HW10

--- src/layout/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Main = () => {
-    return <div className="m-3">Main</div>
-}
-
-export default Main
--- src/utils/pogination.js
+++
@@ -1,3 +0,0 @@
-export default function pogination(items, activePage, pageSize) {
-    return [...items].splice((activePage - 1) * pageSize, pageSize)
-}
